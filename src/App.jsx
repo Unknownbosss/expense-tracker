bf42e4e55489d65cbd0fac07aa1e503cfc8c05ff
UDEIGWE EMMANUEL <92052323+Unknownbosss@users.noreply.github.com>
2024-07-26 14:55:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
